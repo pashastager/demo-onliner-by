5d4f87a156a780674f51c803cf377cbacd8bfa68
pavel <pashastager@gmail.com>
2024-10-31 20:29:05 +0300

    add page object,messagess

---
+++ demo-onliner-by/src/main/java/Helpers/OnlinerLoginPageMessages.java
@@ -0,0 +1,8 @@
+package Helpers;
+
+public class OnlinerLoginPageMessages {
+    public static final String ENTER_NICKNAME_ERROR = "Укажите ник или e-mail";
+    public static final String ENTER_PASSWORD_ERROR = "Укажите пароль";
+    public static final String LOGIN_AND_PASSWORD_ERROR_XPATH = "Неверный логин или пароль";
+
+}
---
+++ demo-onliner-by/src/main/java/Helpers/OnlinerLoginPageXpath.java
@@ -0,0 +1,12 @@
+package Helpers;
+
+public class OnlinerLoginPageXpath {
+    public static final String OPEN_LOGIN_PAGE_BUTTON_XPATH = "//*[@id=\"userbar\"]/div[2]/div/div/div[1]";
+    public static final String INPUT_LOGIN_XPATH = "//*[@id=\"auth-container\"]/div/div[2]/div/form/div[1]/div/div[2]/div/div/div/div/input";
+    public static final String INPUT_PASSWORD_XPATH = "//*[@id=\"auth-container\"]/div/div[2]/div/form/div[2]/div/div/div/div/input";
+    public static final String LOGIN_BUTTON_XPATH = "//*[@id=\"auth-container\"]/div/div[2]/div/form/div[3]/button";
+    public static final String ENTER_NICKNAME_ERROR_XPATH = "//*[@id=\"auth-container\"]/div/div[2]/div/form/div[1]/div/div[2]/div/div/div[2]/div";
+    public static final String ENTER_PASSWORD_ERROR_XPATH = "//*[@id=\"auth-container\"]/div/div[2]/div/form/div[2]/div/div/div[2]/div";
+    public static final String LOGIN_AND_PASSWORD_ERROR_XPATH = "//*[@id=\"auth-container\"]/div/div[2]/div/form/div[3]/div";
+}
+
