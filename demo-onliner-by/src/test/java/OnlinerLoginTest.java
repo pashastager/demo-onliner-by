c42e29e53fb0f7a4098979d12122793d5a3bad6f
pavel <pashastager@gmail.com>
2024-10-31 21:11:35 +0300

    add test errorWitoutPassword

--- demo-onliner-by/src/test/java/OnlinerLoginTest.java
+++ demo-onliner-by/src/test/java/OnlinerLoginTest.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import java.time.Duration;
 
 import static Helpers.OnlinerLoginPageMessages.ENTER_NICKNAME_ERROR;
+import static Helpers.OnlinerLoginPageMessages.ENTER_PASSWORD_ERROR;
 import static Helpers.OnlinerLoginPageXpath.*;
 
 public class OnlinerLoginTest {
@@ -21,5 +22,18 @@ public class OnlinerLoginTest {
         onlinerLoginPageMethods.sendKeysInput(INPUT_PASSWORD_XPATH, "Qwd12345#");
         onlinerLoginPageMethods.clickButton(LOGIN_BUTTON_XPATH);
         Assertions.assertEquals(onlinerLoginPageMethods.getResultErrorText(ENTER_NICKNAME_ERROR_XPATH), ENTER_NICKNAME_ERROR);
+    }
+
+    @Test
+    public void errorWitoutPassword() {
+        WebDriver webDriver = new ChromeDriver();
+        webDriver.get("https://www.onliner.by");
+        webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        OnlinerLoginPageMethods onlinerLoginPageMethods = new OnlinerLoginPageMethods(webDriver);
+
+        onlinerLoginPageMethods.clickButton(OPEN_LOGIN_PAGE_BUTTON_XPATH);
+        onlinerLoginPageMethods.sendKeysInput(INPUT_LOGIN_XPATH, "Pavelkalinkov");
+        onlinerLoginPageMethods.clickButton(LOGIN_BUTTON_XPATH);
+        Assertions.assertEquals(onlinerLoginPageMethods.getResultErrorText(ENTER_PASSWORD_ERROR_XPATH), ENTER_PASSWORD_ERROR);
     }
 }
