1e2284ec981c938c31b2356d06134571ec47021a
pavel <pashastager@gmail.com>
2024-10-31 20:53:49 +0300

    add test errorWitoutLogin

--- demo-onliner-by/src/main/java/Helpers/OnlinerLoginPageMethods.java
+++ demo-onliner-by/src/main/java/Helpers/OnlinerLoginPageMethods.java
@@ -1,4 +1,33 @@
 package Helpers;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 public class OnlinerLoginPageMethods {
+    private WebDriver webDriver;
+
+    public OnlinerLoginPageMethods(WebDriver webDriver) {
+        this.webDriver = webDriver;
+    }
+
+    public void clickButton(String button) {
+
+        By by = By.xpath(button);
+        WebElement webElement = webDriver.findElement(by);
+        webElement.click();
+    }
+
+    public void sendKeysInput(String field, String value) {
+
+        By inputNameBy = By.xpath(field);
+        WebElement inputWebElement = webDriver.findElement(inputNameBy);
+        inputWebElement.sendKeys(value);
+    }
+
+    public String getResultErrorText(String textXpath) {
+        By textResultErrorBy = By.xpath(textXpath);
+        WebElement textResultErrorWebElement = webDriver.findElement(textResultErrorBy);
+        return textResultErrorWebElement.getText();
+    }
 }
--- demo-onliner-by/src/test/java/OnlinerLoginTest.java
+++ demo-onliner-by/src/test/java/OnlinerLoginTest.java
@@ -1,2 +1,24 @@
+import Helpers.OnlinerLoginPageMethods;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static Helpers.OnlinerLoginPageMessages.ENTER_NICKNAME_ERROR;
+import static Helpers.OnlinerLoginPageXpath.*;
+
 public class OnlinerLoginTest {
+    @Test
+    public void errorWitoutLogin() {
+        WebDriver webDriver = new ChromeDriver();
+        webDriver.get("https://www.onliner.by/");
+        OnlinerLoginPageMethods onlinerLoginPageMethods = new OnlinerLoginPageMethods(webDriver);
+
+        onlinerLoginPageMethods.clickButton(OPEN_LOGIN_PAGE_BUTTON_XPATH);
+        onlinerLoginPageMethods.sendKeysInput(INPUT_PASSWORD_XPATH, "Qwd12345#");
+        onlinerLoginPageMethods.clickButton(LOGIN_BUTTON_XPATH);
+        String actualResult = onlinerLoginPageMethods.getResultErrorText(ENTER_NICKNAME_ERROR_XPATH);
+        String expectedResult = ENTER_NICKNAME_ERROR;
+        Assertions.assertEquals(actualResult, expectedResult);
+    }
 }
