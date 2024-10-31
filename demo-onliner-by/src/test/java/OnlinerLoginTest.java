f2e531502981c63f1b957b914e7927209cd03d34
pavel <pashastager@gmail.com>
2024-10-31 21:07:56 +0300

    edit test errorWitoutLogin

--- demo-onliner-by/src/test/java/OnlinerLoginTest.java
+++ demo-onliner-by/src/test/java/OnlinerLoginTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.time.Duration;
+
 import static Helpers.OnlinerLoginPageMessages.ENTER_NICKNAME_ERROR;
 import static Helpers.OnlinerLoginPageXpath.*;
 
@@ -11,14 +13,13 @@ public class OnlinerLoginTest {
     @Test
     public void errorWitoutLogin() {
         WebDriver webDriver = new ChromeDriver();
-        webDriver.get("https://www.onliner.by/");
+        webDriver.get("https://www.onliner.by");
+        webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         OnlinerLoginPageMethods onlinerLoginPageMethods = new OnlinerLoginPageMethods(webDriver);
 
         onlinerLoginPageMethods.clickButton(OPEN_LOGIN_PAGE_BUTTON_XPATH);
         onlinerLoginPageMethods.sendKeysInput(INPUT_PASSWORD_XPATH, "Qwd12345#");
         onlinerLoginPageMethods.clickButton(LOGIN_BUTTON_XPATH);
-        String actualResult = onlinerLoginPageMethods.getResultErrorText(ENTER_NICKNAME_ERROR_XPATH);
-        String expectedResult = ENTER_NICKNAME_ERROR;
-        Assertions.assertEquals(actualResult, expectedResult);
+        Assertions.assertEquals(onlinerLoginPageMethods.getResultErrorText(ENTER_NICKNAME_ERROR_XPATH), ENTER_NICKNAME_ERROR);
     }
 }
